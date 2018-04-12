f2d79d2c3f414792e541b7719549c63fc1ac3ea5
DESKTOP-S41QJ4N\zhaoyiming <912778422@qq.com>
2018-04-12 21:09:13 +0800

    base imp( with bugssss)

---
+++ Assets/zMarchingCube/Generator.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+
+public class Generator : MonoBehaviour {
+
+    public Material m_material;
+    public int sizeScale = 1;
+    public int seed = 0;
+    public bool useJobSystem;
+    List<GameObject> meshes = new List<GameObject>();
+
+    List<Vector3> verts;
+    List<int> indices;
+    int width  ;
+    int height ;
+    int length;
+    void Generate() {
+
+        DateTime startTime = System.DateTime.Now;
+
+        INoise perlin = new PerlinNoise(seed, 2f);
+        FractalNoise fractal = new FractalNoise(perlin, 3, 1f);
+
+        MarchingCube marching = new MarchingCube();
+        Marching.Surface = 0f;
+
+        width =  sizeScale;
+        height =sizeScale;
+        length = sizeScale;
+
+        float[] voxels = new float[width * height * length];
+
+        //Fill voxels with values. Im using perlin noise but any method to create voxels will work.
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int z = 0; z < length; z++)
+                {
+                    float fx = x / (width - 1.0f);
+                    float fy = y / (height - 1.0f);
+                    float fz = z / (length - 1.0f);
+
+                    int idx = x + y * width + z * width * height;
+
+                    voxels[idx] = fractal.Sample3D(fx, fy, fz);
+                }
+            }
+        }
+
+        verts = new List<Vector3>();
+        indices = new List<int>();
+
+        
+        if(useJobSystem)
+        {
+            MarchingCubeParallel marching_p = new MarchingCubeParallel(0);
+            MarchingCubeParallel.MarchJob job;
+            JobHandle handle = marching_p.Generate(new List<float>(voxels), width, height, length, out job);
+            StartCoroutine(Wait4Complete(handle, job));            
+        }   
+        else 
+        {
+            marching.Generate(voxels, width, height, length, verts, indices);
+            int maxVertsPerMesh = 30000; //must be divisible by 3, ie 3 verts == 1 triangle
+            int numMeshes = verts.Count / maxVertsPerMesh + 1;
+            for (int i = 0; i < numMeshes; i++)
+            {
+
+                List<Vector3> splitVerts = new List<Vector3>();
+                List<int> splitIndices = new List<int>();
+
+                for (int j = 0; j < maxVertsPerMesh; j++)
+                {
+                    int idx = i * maxVertsPerMesh + j;
+
+                    if (idx < verts.Count)
+                    {
+                        splitVerts.Add(verts[idx]);
+                        splitIndices.Add(j);
+                    }
+                }
+
+                if (splitVerts.Count == 0) continue;
+
+                Mesh mesh = new Mesh();
+                mesh.SetVertices(splitVerts);
+                mesh.SetTriangles(splitIndices, 0);
+                mesh.RecalculateBounds();
+                mesh.RecalculateNormals();
+
+                GameObject go = new GameObject("Mesh");
+                go.transform.parent = transform;
+                go.AddComponent<MeshFilter>();
+                go.AddComponent<MeshRenderer>();
+                go.GetComponent<Renderer>().material = m_material;
+                go.GetComponent<MeshFilter>().mesh = mesh;
+                go.transform.localPosition = new Vector3(-width / 2, -height / 2, -length / 2);
+
+                meshes.Add(go);
+            }
+        
+        }
+        double timeCost = System.DateTime.Now.Subtract(startTime).TotalMilliseconds;
+        print("Time Cost : " + timeCost + " at size " + (8 * sizeScale));
+    }
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.G))
+            Generate();
+
+    }
+
+    IEnumerator Wait4Complete(JobHandle handle, MarchingCubeParallel.MarchJob job)
+    {
+        while (!handle.IsCompleted)
+            yield return null;
+
+        handle.Complete();
+        verts = new List<Vector3>(MarchingCubeParallel.m_verts);
+        print("vert length " + verts.Count);
+        indices = new List<int>(MarchingCubeParallel.m_indices);
+        print("indices length " + indices.Count);
+        //if (job.m_indices.IsCreated)
+        //    job.m_indices.Dispose();
+        //if (job.m_verts.IsCreated)
+        //    job.m_verts.Dispose();
+
+        int maxVertsPerMesh = 60000; //must be divisible by 3, ie 3 verts == 1 triangle
+        int numMeshes = verts.Count / maxVertsPerMesh + 1;
+        for (int i = 0; i < numMeshes; i++)
+        {
+
+            List<Vector3> splitVerts = new List<Vector3>();
+            List<int> splitIndices = new List<int>();
+
+            for (int j = 0; j < maxVertsPerMesh; j++)
+            {
+                int idx = i * maxVertsPerMesh + j;
+
+                if (idx < verts.Count)
+                {
+                    splitVerts.Add(verts[idx]);
+                    splitIndices.Add(j);
+                }
+            }
+
+            if (splitVerts.Count == 0) continue;
+
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(splitVerts);
+            mesh.SetTriangles(splitIndices, 0);
+            mesh.RecalculateBounds();
+            mesh.RecalculateNormals();
+
+            GameObject go = new GameObject("Mesh");
+            go.transform.parent = transform;
+            go.AddComponent<MeshFilter>();
+            go.AddComponent<MeshRenderer>();
+            go.GetComponent<Renderer>().material = m_material;
+            go.GetComponent<MeshFilter>().mesh = mesh;
+            go.transform.localPosition = new Vector3(-width / 2, -height / 2, -length / 2);
+
+            meshes.Add(go);
+        }
+    }
+}
---
+++ Assets/zMarchingCube/JobSystem/MarchingCubeParallel.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using System;
+
+public class MarchingCubeParallel
+{
+    public static float Surface { get; set; }
+
+    /// <summary>
+    /// Winding order of triangles use 2,1,0 or 0,1,2
+    /// </summary>
+    protected static int[] WindingOrder { get; private set; }
+
+    //private static Vector3[] EdgeVertex { get; set; }
+
+    private static int currentVertIndex;
+
+    private static float[] m_voxels;
+
+    public static Vector3[] m_verts;
+
+    public static int[] m_indices;
+
+    public MarchingCubeParallel(float surface = 0.5f)
+    {
+        Surface = surface;
+        WindingOrder = new int[] { 0, 1, 2 };
+        //EdgeVertex = new Vector3[12];
+    }
+
+    public struct MarchJob : IJobParallelFor
+    {
+        public int w, h, d;
+        public int ix, iy, iz;
+        //public NativeArray<Vector3> m_verts;
+        //public NativeArray<int> m_indices;        
+        public void Execute(int index)
+        {
+            int x = index / (h * d);
+            int y = (index - x * h * d) / d;
+            int z = (index - x * h * d) % h;
+
+            float[] Cube = new float[8];
+            for (int ii = 0; ii < 8; ii++)
+            {
+                ix = x + Marching.VertexOffset[ii, 0];
+                iy = y + Marching.VertexOffset[ii, 1];
+                iz = z + Marching.VertexOffset[ii, 2];
+                Cube[ii] = m_voxels[ix + iy * w + iz * w * h];
+            }
+
+            //perform algorithm
+            int i, j, vert, idx;
+            int flagIndex = 0;
+            float offset = 0f;
+            NativeArray<Vector3> EdgeVertex = new NativeArray<Vector3>(12, Allocator.TempJob);
+
+            for (i = 0; i < 8; i++)
+            {
+                //Debug.Log("Cube[i] " + Cube[i] + " Surface " + Surface);
+                if (Cube[i] <= Surface)
+                    flagIndex |= 1 << i;
+            }
+            //Debug.Log("b4 intersection : " + flagIndex);
+            int edgeFlags = MarchingCube.CubeEdgeFlags[flagIndex];
+            if (edgeFlags == 0) return;
+
+            //Debug.Log("Got intersection");
+            for (i = 0; i < 12; i++)
+            {
+                //intersection happens
+                if((edgeFlags & (1 << i)) != 0)
+                {
+                    offset = Marching.GetOffset(Cube[MarchingCube.EdgeConnection[i, 0]], Cube[MarchingCube.EdgeConnection[i, 1]]);
+
+                    EdgeVertex[i] = new Vector3(x + (Marching.VertexOffset[MarchingCube.EdgeConnection[i, 0], 0]
+                        + offset * MarchingCube.EdgeDirection[i, 0]),
+                    y + (Marching.VertexOffset[MarchingCube.EdgeConnection[i, 0], 1]
+                        + offset * MarchingCube.EdgeDirection[i, 1]),
+                    z + (Marching.VertexOffset[MarchingCube.EdgeConnection[i, 0], 2]
+                        + offset * MarchingCube.EdgeDirection[i, 2]));
+                }
+            }
+
+            for (i = 0; i < 5; i++)
+            {
+                if (MarchingCube.TriangleConnectionTable[flagIndex, 3 * i] < 0) break;
+
+                //idx = x * w * d + y * d  + z;
+                for ( j = 0; j < 3; j++)
+                {
+                    vert = MarchingCube.TriangleConnectionTable[flagIndex, 3 * i + j];
+                    m_indices[currentVertIndex] = currentVertIndex + WindingOrder[j];
+                    m_verts[currentVertIndex++] = EdgeVertex[vert];
+                   // Debug.Log("zzz " + currentVertIndex);
+                }
+            }
+
+            EdgeVertex.Dispose();
+        }
+    }
+
+    private MarchJob marchJob;
+    public JobHandle Generate(List<float> voxels, int width, int height, int depth, out MarchJob job)
+    {
+        if (Surface > 0.0f)
+        {
+            WindingOrder[0] = 0;
+            WindingOrder[1] = 1;
+            WindingOrder[2] = 2;
+        }
+        else
+        {
+            WindingOrder[0] = 2;
+            WindingOrder[1] = 1;
+            WindingOrder[2] = 0;
+        }
+
+        int maxLength = width * height * depth * 15;
+        currentVertIndex = 0;
+        m_voxels = voxels.ToArray();
+        m_verts = new Vector3[maxLength];
+        m_indices = new int[maxLength];
+
+        marchJob = new MarchJob()
+        {
+            w = width - 1,
+            h = height - 1,
+            d = depth - 1          
+            //m_verts = new NativeArray<Vector3>(maxLength, Allocator.Temp),
+            //m_indices = new NativeArray<int>(maxLength, Allocator.Temp)
+        };
+
+        var handle = marchJob.Schedule(width * height * depth, 64);
+        job = marchJob;
+        return handle;       
+    }
+}
